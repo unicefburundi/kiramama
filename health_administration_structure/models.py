36da6acb57078fff3c73ecf7a327a2baf9349c72
mbanje <mbanjejvernack@gmail.com>
2016-07-28 15:00:05 +0200

    Edit models for administrative structures

--- health_administration_structure/models.py
+++ health_administration_structure/models.py
@@ -2,4 +2,50 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class BPS(models.Model):
+    '''In this model, we will store Burundi BPSs(Bureau provincial de la sante)'''
+    name = models.CharField(_('name'),unique=True, max_length=20)
+    code = models.IntegerField(unique=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('province_detail', kwargs={'pk': self.id})
+        return self.id
+
+    class Meta:
+        ordering = ('name',)
+
+class District(models.Model):
+    '''In this model, we will store districts'''
+    bps = models.ForeignKey(BPS, verbose_name='BPS')
+    name = models.CharField(_('nom'), unique=True, max_length=40)
+    code = models.IntegerField(unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+class CDS(models.Model):
+    STATUS_CHOICES = (
+        ('Pub', 'Public'),
+        ('Con', 'Conf'),
+        ('Priv', 'Prive'),
+        ('Ass', 'Ass'),
+        ('HPub', 'HPublic'),
+        ('HCon', 'HConf'),
+        ('HPrv', 'HPrive'),
+    )
+    district = models.ForeignKey(District)
+    name = models.CharField( max_length=40)
+    code = models.CharField(unique=True, max_length=6)
+    status = models.CharField(max_length=4, choices=STATUS_CHOICES, blank=True, null=True, help_text=_('Either Public, Conf, Ass, Prive  or Hospital status.'))
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
--- public_administration_structure/models.py
+++ public_administration_structure/models.py
@@ -2,4 +2,65 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Province(models.Model):
+    '''In this model, we will store burundi provinces'''
+    name = models.CharField(_('name'),unique=True, max_length=20)
+    code = models.IntegerField(unique=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('province_detail', kwargs={'pk': self.id})
+        return self.id
+
+    class Meta:
+        ordering = ('name',)
+
+class Commune(models.Model):
+    '''In this model, we will store burundi communes'''
+    province = models.ForeignKey(Province)
+    name = models.CharField(_('name'),unique=True, max_length=20)
+    code = models.IntegerField(unique=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('province_detail', kwargs={'pk': self.id})
+        return self.id
+
+    class Meta:
+        ordering = ('name',)
+
+class Colline(models.Model):
+    '''In this model, we will store burundi colline'''
+    commune = models.ForeignKey(Commune)
+    name = models.CharField(_('name'), max_length=30)
+    code = models.IntegerField(unique=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('province_detail', kwargs={'pk': self.id})
+        return self.id
+
+    class Meta:
+        ordering = ('name',)
+
+class SousColline(models.Model):
+    '''In this model, we will store burundi sub hills'''
+    colline = models.ForeignKey(Colline)
+    name = models.CharField(_('name'), max_length=30)
+    code = models.IntegerField(unique=True, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # return reverse('province_detail', kwargs={'pk': self.id})
+        return self.id
+
+    class Meta:
+        ordering = ('name',)
