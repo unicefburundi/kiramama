87493aa7080318ccb5c1e7a438be77f691c01e0e
mbanje <mbanjejvernack@gmail.com>
2016-10-06 14:30:44 +0200

    Add static and media folders

--- kiramama/settings.py
+++ kiramama/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^xb=b*1$z_m@!7g19heh)=xk*^h4%z+1c@_u1-9)q3yejsw^#)'
+
+PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            os.pardir))
+PROJECT_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, os.pardir))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +119,20 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(PROJECT_PATH,  'media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_PATH,  'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 
 KNOWN_PREFIXES = {
