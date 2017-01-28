8ef1b998e84a52a7cca4b1526774fbeaad9e5918
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-01-28 09:17:19 +0200

    date picker works

--- staticfiles/js/childhealth.js
+++ staticfiles/js/childhealth.js
@@ -17,7 +17,6 @@ app.controller('ChildCtrl', ['$scope', '$http', function($scope, $http) {
               $scope.districts = response.data.results;
               $http.get("/kiramama/reportnsc/?province=" + province.code )
               .then(function (response) {
-                console.log(response.data.results);
                 $scope.results = response.data.results;
             });
           });
@@ -32,22 +31,45 @@ app.controller('ChildCtrl', ['$scope', '$http', function($scope, $http) {
                 $scope.cdss = response.data.results;
                 $http.get("/kiramama/reportnsc/?district=" + district.code )
                 .then(function (response) {
-                  console.log(response.data.results);
                   $scope.results = response.data.results;
               });
             });
           }
       };
-        // CDS
-        $scope.update_cds = function () {
-          var cds = $scope.cds;
-          if (cds) {
-            $http.get("/kiramama/reportnsc/?cds=" + cds.code )
-            .then(function (response) {
-              console.log(response.data.results);
-              $scope.results = response.data.results;
-          });
-        }
-    };
-}]);
+      // cds
+          $scope.update_cds = function () {
+            var cds = $scope.cds;
+            if (cds) {
+                $http.get("/kiramama/reportnsc/?cds=" + cds.code )
+                .then(function (response) {
+                  $scope.results = response.data.results;
+              });
+          }
+      };
 
+        // date
+        $scope.get_bydate = function () {
+          var startdate = $scope.startdate;
+          var enddate = $scope.enddate;
+          if (startdate) {
+                if (enddate) {      
+                    $http.get("/kiramama/reportnsc/?min_birth_date=" + startdate + "&max_birth_date=" + enddate )
+                      .then(function (response) {
+                          $scope.results = response.data.results;
+                      });
+                } else {
+                    $http.get("/kiramama/reportnsc/?min_birth_date=" + startdate)
+                      .then(function (response) {
+                          $scope.results = response.data.results;
+                      });
+                }
+          } else {
+              if (enddate) {
+                $http.get("/kiramama/reportnsc/?max_birth_date=" + enddate )
+                      .then(function (response) {
+                          $scope.results = response.data.results;
+                      });
+              }
+          }
+      };
+  }]);
